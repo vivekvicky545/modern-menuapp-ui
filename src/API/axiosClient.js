b64c577c2fe738485c3720acd2d31aabf29640f6
vivekvicky545 <131896557+vivekvicky545@users.noreply.github.com>
2023-09-25 19:08:20 +0530

    added public ip of java build

--- src/API/axiosClient.js
+++ src/API/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosClient = axios.create();
 
-axiosClient.defaults.baseURL = "http://localhost:8090/";
+axiosClient.defaults.baseURL = "http://54.158.185.161:8090/";
 
 axiosClient.defaults.headers = {
   "Content-Type": "application/json",
